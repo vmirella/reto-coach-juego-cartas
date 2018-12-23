d0fd09c2c34484f4d59fb86de7e8513c3391b470
vmirella <vmirella_17@hotmail.com>
2018-12-23 03:04:45 -0500

    Implementación de función que genera 4 cartas al azar

--- js/app.js
+++ js/app.js
@@ -5,15 +5,15 @@ Ejemplo de carta: 4D - 4T - 13E - 5C
  */
 
  const generateNumCard = () => {
-  numCard = console.log(Math.round(Math.random() * (13 - 1) + 1));
+  numCard = Math.round(Math.random() * (13 - 1) + 1);
   return numCard;
  }
 
  const generateStickCard = () => {
   //Donde c = 1, D = 2, E = 3 y T = 4
-  numStickCard = console.log(Math.round(Math.random() * (4 - 1) + 1));
-
-  switch(stickCard) {
+  numStickCard = Math.round(Math.random() * (4 - 1) + 1);
+  let stickCard;
+  switch(numStickCard) {
     case 1: stickCard = 'C'; break;
     case 2: stickCard = 'D'; break;
     case 3: stickCard = 'E'; break;
@@ -23,7 +23,25 @@ Ejemplo de carta: 4D - 4T - 13E - 5C
 }
  //Devuelve un arreglo con 4 cartas no repetidas
  const generateCards = () => {
+  const arrayFourCards = [];
   
+  for (let i = 0; i < 4; i++){
+    let flag = 0; //0- no encontrado, 1 - es encontrado
+    do {
+      let numCard = generateNumCard();
+      let stickCard = generateStickCard();
+      let valueCard = numCard + stickCard;
+      if(arrayFourCards.indexOf(valueCard) === -1) {
+        arrayFourCards.push(valueCard);
+        flag = 0;
+      }
+      else {
+        flag = 1;
+      }
+    }
+    while (flag === 1);
+  }
+  return arrayFourCards;
 }
 
  generateCards();
